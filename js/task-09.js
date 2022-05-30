4f8499e37ba858836273451f030447c6c6aaaaef
Serhii Boiko <1.serhiiboiko.1@gmail.com>
2022-05-30 19:06:24 +0300

    Task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  body: document.querySelector('body'),
+  changeColor: document.querySelector('.change-color'),
+  textColor: document.querySelector('.color'),
+};
+
+refs.changeColor.addEventListener('click', changeColor);
+
+function changeColor() {
+  const currentColor = getRandomHexColor();
+  refs.body.style.backgroundColor = currentColor;
+  refs.textColor.textContent = currentColor
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
