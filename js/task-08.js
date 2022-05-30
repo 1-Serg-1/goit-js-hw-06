8734a5bce0c1f49d8424bc50e56e78e35492c42d
Serhii Boiko <1.serhiiboiko.1@gmail.com>
2022-05-30 19:06:14 +0300

    Task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+console.log(form);
+
+form.addEventListener('submit', onFormSubmit);
+
+let user = {
+    email: '',
+    password: '',
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formEl = event.currentTarget.elements;
+    if (formEl.email.value === "" || formEl.password.value === "") {
+        return alert("Все поля должны быть заполнены!");
+    } else {
+        user.email = formEl.email.value;
+        user.password = formEl.password.value;
+    }
+    event.currentTarget.reset();
+
+    console.log(user);
+};
 
